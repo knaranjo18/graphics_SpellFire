979278263d1e0f4b849965fc6ea91e5ddd0f2dc7
Kevin Naranjo <knaranjo18@gmail.com>
2020-05-02 21:25:57 -0400

    Cleaned up some extra files

--- gfxDefs.h
+++ gfxDefs.h
@@ -1,6 +1,9 @@
-#pragma once
+#ifndef DEFS_H
+#define DEFS_H
 
 const float EPSILON = 1e-5;
 #define PI glm::pi<float>()
 #define IN_RANGE(a,b)   (((a>(b-EPSILON))&&(a<(b+EPSILON)))?1:0)
 #define TO_RADIANS(a)   (a*PI/180.0f)
+
+#endif
